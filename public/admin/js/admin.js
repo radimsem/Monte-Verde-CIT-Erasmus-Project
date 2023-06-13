0131f4bd5f0aff7f10308ea48933956aeb205cb9
radimsem <radim.semerak@gmail.com>
2023-06-13 10:24:02 +0100

    Complete Admin Design Update

--- public/admin/js/admin.js
+++ public/admin/js/admin.js
@@ -1,5 +1,11 @@
+const artsLink = document.querySelector(".articles_link a");
+const dropdown = document.querySelector(".dropdown");
 const closeBtn = document.querySelector(".close_btn");
 const navAside = document.querySelector(".navigation_aside");
+
+artsLink.addEventListener("click", () => {
+  dropdown.classList.toggle("active");
+})
 
 closeBtn.addEventListener("click", () => {
   navAside.classList.toggle("closed");
